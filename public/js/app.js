aa33d28df488a37a9d40a593c1fb931770b23826
tuli.deepak@gmail.com <DeepakTuli1!>
2020-08-05 06:15:12 +0530

    Balloon Block Editor added

--- public/js/app.js
+++ public/js/app.js
@@ -19355,8 +19355,6 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * for events that are broadcast by Laravel. Echo and event broadcasting
  * allows your team to easily build robust real-time web applications.
  */
-// var ClassicEditor = require('@ckeditor/ckeditor5-build-classic');
-// import {ClassicEditor} from '@ckeditor/ckeditor5-build-classic'
 // import Echo from 'laravel-echo';
 // window.Pusher = require('pusher-js');
 // window.Echo = new Echo({
